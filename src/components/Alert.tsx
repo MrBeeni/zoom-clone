492733e961dde20d1536edb46db2661fd175bea4
MrBeeni <mubeen4429@gmail.com>
2024-07-15 10:59:31 +0500

    implement join meeting functionality

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -24,8 +24,11 @@ const Alert = ({ title, iconUrl }: PermissionCardProps) => {
               <p className="text-center text-xl font-semibold">{title}</p>
             </div>
 
-            <Button asChild className="bg-blue-1">
-              <Link href="/">Back to Home</Link>
+            <Button
+              className="bg-blue-1"
+              onClick={() => window.location.replace("/")}
+            >
+              Back to Home
             </Button>
           </div>
         </CardContent>
--- src/components/CallListContainer.tsx
+++ src/components/CallListContainer.tsx
@@ -83,7 +83,7 @@ const CallListContainer = ({
             title={
               (meeting as Call).state?.custom?.description ||
               (meeting as CallRecording).filename?.substring(0, 20) ||
-              "No Description"
+              "Personal meeting"
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
